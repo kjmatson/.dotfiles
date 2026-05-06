ca94f3d90566bf1f18c688181eaa7042317c97db
ken <126749091+kjmatson@users.noreply.github.com>
2026-05-06 19:28:55 -0400

    fish and tmux things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #end
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
+
+if type -q tmux
+    if not test -n "$TMUX"
+        tmux attach-session -t default; or tmux new-session -s default
+    end
+end   
